08fd123067b3958e7ccc445dff6cbcf09e05ef9c
khanghoang <hoangtrieukhang@gmail.com>
2014-10-29 12:17:35 +0700

    Add super collection cell factory, changing nib cell to class cell

---
+++ KHTableViewController/Classes/Collection Module/KHSuperCollectionCellFactory.m
@@ -0,0 +1,13 @@
+//
+//  KHSuperCollectionCellFactory.m
+//  KHTableViewController
+//
+//  Created by Triệu Khang on 29/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import "KHSuperCollectionCellFactory.h"
+
+@implementation KHSuperCollectionCellFactory
+
+@end
