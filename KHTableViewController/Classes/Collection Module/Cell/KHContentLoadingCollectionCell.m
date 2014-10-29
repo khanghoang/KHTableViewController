80cb2ebbc389b139009e84e6530ab10cd26d5969
khanghoang <hoangtrieukhang@gmail.com>
2014-10-29 12:17:35 +0700

    Add super collection cell factory, changing nib cell to class cell

--- KHTableViewController/Classes/Collection Module/Cell/KHContentLoadingCollectionCell.m
+++ KHTableViewController/Classes/Collection Module/Cell/KHContentLoadingCollectionCell.m
@@ -10,4 +10,20 @@
 
 @implementation KHContentLoadingCollectionCell
 
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        [indicator startAnimating];
+        [self addSubview:indicator];
+        NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:indicator attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
+        NSLayoutConstraint *centerY = [NSLayoutConstraint constraintWithItem:indicator attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1 constant:0];
+        [self addConstraints:@[centerX, centerY]];
+        [self setNeedsUpdateConstraints];
+        [self layoutIfNeeded];
+    }
+
+    return self;
+}
+
 @end
---
+++ KHTableViewController/Classes/Collection Module/KHSuperCollectionCellFactory.h
@@ -0,0 +1,15 @@
+//
+//  KHSuperCollectionCellFactory.h
+//  KHTableViewController
+//
+//  Created by Triệu Khang on 29/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHSuperCollectionCellFactory : NSObject
+
+
+
+@end
