f42e7b9e3a9cdab9e6d5bf464a67d7f8769221f5
khanghoang <hoangtrieukhang@gmail.com>
2014-10-29 16:02:00 +0700

    Fix the @import

--- KHTableViewController/Classes/ViewModels/KHFluentDataProvider.h
+++ KHTableViewController/Classes/ViewModels/KHFluentDataProvider.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Alek Åström. All rights reserved.
 //
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 @class KHFluentDataProvider;
 @protocol KHDataProviderDelegate<NSObject>
