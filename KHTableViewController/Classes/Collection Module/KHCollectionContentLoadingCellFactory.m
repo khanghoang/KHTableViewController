ff74a5d651da4901bca22d5acf2c91fd323cb247
khanghoang <hoangtrieukhang@gmail.com>
2014-10-30 11:10:29 +0700

    Fix the loading/register cell

--- KHTableViewController/Classes/Collection Module/KHCollectionContentLoadingCellFactory.m
+++ KHTableViewController/Classes/Collection Module/KHCollectionContentLoadingCellFactory.m
@@ -20,21 +20,4 @@
     return collectionView.frame.size;
 }
 
-#pragma mark - Private methods
-
-- (UICollectionViewCell <KHCellProtocol> *)_getReusableCellWithClass:(Class)cellClass collectionView:(UICollectionView *)collectionView atIndexPath:(NSIndexPath *)indexPath{
-    [self _registerTheClass:cellClass toCollectionView:collectionView];
-    return [self _dequeueReuseableCellWithClass:cellClass ofCollectionView:collectionView atIndexPath:indexPath];
-}
-
-- (void)_registerTheClass:(Class)cellClass toCollectionView:(UICollectionView *)collectionView {
-	UINib *cellNib = [UINib nibWithNibName:NSStringFromClass(cellClass) bundle:nil];
-	[collectionView registerNib:cellNib forCellWithReuseIdentifier:NSStringFromClass(cellClass)];
-}
-
-- (UICollectionViewCell <KHCellProtocol> *)_dequeueReuseableCellWithClass:(Class)cellClass ofCollectionView:(UICollectionView *)collectionView atIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell <KHCellProtocol> *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass(cellClass) forIndexPath:indexPath];
-	return cell;
-}
-
 @end
