37944780393a1ca506065dd71f5691878694bb34
khanghoang <hoangtrieukhang@gmail.com>
2014-11-02 22:34:51 +0700

    Remove load more cell when reach the end

--- KHTableViewController/Classes/Collection Module/KHBasicOrderedCollectionViewController.m
+++ KHTableViewController/Classes/Collection Module/KHBasicOrderedCollectionViewController.m
@@ -19,68 +19,70 @@
 @property (strong, nonatomic) LBDelegateMatrioska *chainDelegate;
 
 @property (strong, nonatomic) KHContentLoadingCellFactory *loadingCellFactory;
-@property (strong, nonatomic) id<KHTableViewSectionModel> orderedDataProvider;
+@property (strong, nonatomic) id <KHTableViewSectionModel> orderedDataProvider;
 
 @end
 
 @implementation KHBasicOrderedCollectionViewController
 
 - (void)loadView {
-    [super loadView];
+	[super loadView];
 }
 
 - (void)viewDidLoad {
-    self.collectionController = [[KHCollectionController alloc] init];
+	self.collectionController = [[KHCollectionController alloc] init];
 
-    self.collectionController.cellFactory = self.cellFactory;
+	self.collectionController.cellFactory = self.cellFactory;
 
-    self.collectionView.dataSource = (id)self.collectionController;
-    self.collectionView.delegate = (id)self.collectionController;
+	self.collectionView.dataSource = (id)self.collectionController;
+	self.collectionView.delegate = (id)self.collectionController;
 
-    KHBasicTableViewModel *loadingContentSection = [[KHBasicTableViewModel alloc] init];
-    KHContentLoadingSectionViewModel *loadingSecionModel = [[KHContentLoadingSectionViewModel alloc] init];
-    loadingContentSection.sectionModel = loadingSecionModel;
+	KHBasicTableViewModel *loadingContentSection = [[KHBasicTableViewModel alloc] init];
+	KHContentLoadingSectionViewModel *loadingSecionModel = [[KHContentLoadingSectionViewModel alloc] init];
+	loadingContentSection.sectionModel = loadingSecionModel;
 
-    id<KHTableViewSectionModel> loadingContentViewModel = [self getLoadingContentViewModel];
+	id <KHTableViewSectionModel> loadingContentViewModel = [self getLoadingContentViewModel];
 
-    KHOrderedDataProvider *provider = (KHOrderedDataProvider *) loadingContentViewModel;
-    provider.delegate = self;
-    self.orderedDataProvider = loadingContentViewModel;
+	KHOrderedDataProvider *provider = (KHOrderedDataProvider *)loadingContentViewModel;
+	provider.delegate = self;
+	self.orderedDataProvider = loadingContentViewModel;
 
-    self.basicModel = loadingContentSection;
-    self.collectionController.model = self.basicModel;
+	self.basicModel = loadingContentSection;
+	self.collectionController.model = self.basicModel;
 
-    [self.collectionView reloadData];
+	[self.collectionView reloadData];
 
-    [provider startLoading];
+	[provider startLoading];
 }
 
 - (void)dataProvider:(KHOrderedDataProvider *)dataProvider didLoadDataAtPage:(NSUInteger)page withItems:(NSArray *)items error:(NSError *)error {
+    KHBasicTableViewModel *loadMore = nil;
+	if (!dataProvider.isReachMaxPage) {
+		// create loadmore section
+		loadMore = [[KHBasicTableViewModel alloc] initWithModel:nil];
+		loadMore.sectionModel = [[KHLoadMoreSection alloc] init];
+	}
 
-    // create loadmore section
-    KHBasicTableViewModel *loadMore = [[KHBasicTableViewModel alloc] initWithModel:nil];
-    loadMore.sectionModel = [[KHLoadMoreSection alloc] init];
+	// linked with content section
+	KHBasicTableViewModel *content = [[KHBasicTableViewModel alloc] initWithModel:loadMore];
+	content.sectionModel = self.orderedDataProvider;
 
-    // linked with content section
-    KHBasicTableViewModel *content = [[KHBasicTableViewModel alloc] initWithModel:loadMore];
-    content.sectionModel = self.orderedDataProvider;
+	// set back to controller model
+	self.collectionController.model = content;
 
-    // set back to controller model
-    self.collectionController.model = content;
-
-    [self.collectionView reloadData];
+	[self.collectionView reloadData];
 }
 
-- (id<KHTableViewSectionModel>)getLoadingContentViewModel {
-    return nil;
+- (id <KHTableViewSectionModel> )getLoadingContentViewModel {
+	return nil;
 }
 
 - (id <KHLoadingOperationProtocol> )loadingOperationForSectionViewModel:(id <KHTableViewSectionModel> )viewModel forPage:(NSUInteger)page {
-    return nil;
+	return nil;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return nil;
+	return nil;
 }
 
 @end
