bde169afb72f0a86426e2f1a9cffb16b7b2ca09c
khanghoang <hoangtrieukhang@gmail.com>
2014-11-03 10:33:56 +0700

    Add pull to refresh to basic orderd collection

--- KHTableViewController/Classes/Collection Module/KHBasicOrderedCollectionViewController.m
+++ KHTableViewController/Classes/Collection Module/KHBasicOrderedCollectionViewController.m
@@ -10,8 +10,10 @@
 
 @interface KHBasicOrderedCollectionViewController ()
 <
-    KHOrderedDataProtocol
+KHOrderedDataProtocol
 >
+
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (strong, nonatomic) KHCollectionController *collectionController;
@@ -26,63 +28,81 @@
 @implementation KHBasicOrderedCollectionViewController
 
 - (void)loadView {
-	[super loadView];
+    [super loadView];
 }
 
 - (void)viewDidLoad {
-	self.collectionController = [[KHCollectionController alloc] init];
+    self.collectionController = [[KHCollectionController alloc] init];
 
-	self.collectionController.cellFactory = self.cellFactory;
+    self.collectionController.cellFactory = self.cellFactory;
 
-	self.collectionView.dataSource = (id)self.collectionController;
-	self.collectionView.delegate = (id)self.collectionController;
+    self.collectionView.dataSource = (id)self.collectionController;
+    self.collectionView.delegate = (id)self.collectionController;
 
-	KHBasicTableViewModel *loadingContentSection = [[KHBasicTableViewModel alloc] init];
-	KHContentLoadingSectionViewModel *loadingSecionModel = [[KHContentLoadingSectionViewModel alloc] init];
-	loadingContentSection.sectionModel = loadingSecionModel;
-
-	id <KHTableViewSectionModel> loadingContentViewModel = [self getLoadingContentViewModel];
-
-	KHOrderedDataProvider *provider = (KHOrderedDataProvider *)loadingContentViewModel;
-	provider.delegate = self;
-	self.orderedDataProvider = loadingContentViewModel;
-
-	self.basicModel = loadingContentSection;
-	self.collectionController.model = self.basicModel;
-
-	[self.collectionView reloadData];
-
-	[provider startLoading];
+    [self _onRefresh];
 }
 
 - (void)dataProvider:(KHOrderedDataProvider *)dataProvider didLoadDataAtPage:(NSUInteger)page withItems:(NSArray *)items error:(NSError *)error {
     KHBasicTableViewModel *loadMore = nil;
-	if (!dataProvider.isReachMaxPage) {
-		// create loadmore section
-		loadMore = [[KHBasicTableViewModel alloc] initWithModel:nil];
-		loadMore.sectionModel = [[KHLoadMoreSection alloc] init];
-	}
+    if (!dataProvider.isReachMaxPage) {
+        // create loadmore section
+        loadMore = [[KHBasicTableViewModel alloc] initWithModel:nil];
+        loadMore.sectionModel = [[KHLoadMoreSection alloc] init];
+    }
 
-	// linked with content section
-	KHBasicTableViewModel *content = [[KHBasicTableViewModel alloc] initWithModel:loadMore];
-	content.sectionModel = self.orderedDataProvider;
+    // linked with content section
+    KHBasicTableViewModel *content = [[KHBasicTableViewModel alloc] initWithModel:loadMore];
+    content.sectionModel = self.orderedDataProvider;
 
-	// set back to controller model
-	self.collectionController.model = content;
+    // set back to controller model
+    self.collectionController.model = content;
 
-	[self.collectionView reloadData];
+    [self.collectionView reloadData];
 }
 
 - (id <KHTableViewSectionModel> )getLoadingContentViewModel {
-	return nil;
+    return nil;
 }
 
 - (id <KHLoadingOperationProtocol> )loadingOperationForSectionViewModel:(id <KHTableViewSectionModel> )viewModel forPage:(NSUInteger)page {
-	return nil;
+    return nil;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-	return nil;
+    return nil;
+}
+
+#pragma mark - Public
+
+- (void)enablePullToRefresh {
+    [self _addPullRefresh];
+}
+
+#pragma mark - Pull refresh
+
+- (void)_addPullRefresh {
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.collectionView addSubview:self.refreshControl];
+    [self.refreshControl addTarget:self action:@selector(_onRefresh) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void)_onRefresh {
+    KHBasicTableViewModel *loadingContentSection = [[KHBasicTableViewModel alloc] init];
+    KHContentLoadingSectionViewModel *loadingSecionModel = [[KHContentLoadingSectionViewModel alloc] init];
+    loadingContentSection.sectionModel = loadingSecionModel;
+
+    id <KHTableViewSectionModel> loadingContentViewModel = [self getLoadingContentViewModel];
+
+    KHOrderedDataProvider *provider = (KHOrderedDataProvider *)loadingContentViewModel;
+    provider.delegate = self;
+    self.orderedDataProvider = loadingContentViewModel;
+
+    self.basicModel = loadingContentSection;
+    self.collectionController.model = self.basicModel;
+    
+    [self.collectionView reloadData];
+    
+    [provider startLoading];
 }
 
 @end
