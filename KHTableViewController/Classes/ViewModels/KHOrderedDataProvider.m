0f9848986bf879f3f30104268e97093967632aca
khanghoang <hoangtrieukhang@gmail.com>
2014-11-13 16:10:27 +0700

    Fix bug about revert in ordered data provider

--- KHTableViewController/Classes/ViewModels/KHOrderedDataProvider.m
+++ KHTableViewController/Classes/ViewModels/KHOrderedDataProvider.m
@@ -8,7 +8,7 @@
 
 #import "KHOrderedDataProvider.h"
 
-@interface KHOrderedDataProvider()
+@interface KHOrderedDataProvider ()
 
 @property (assign, nonatomic) NSInteger currentPage;
 @property (assign, nonatomic) BOOL isLoadingNextPage;
@@ -35,31 +35,23 @@
 #pragma mark - KHTableViewSectionModel interface
 
 - (NSUInteger)count {
-	return [self.arrItems count];
+    return [self.arrItems count];
 }
 
 - (id)objectAtIndex:(NSUInteger)index {
-	if (self.reversed) {
-		return [self _objectAtIndex:(self.arrItems.count - index - 1)];
-	}
-
-    return [self _objectAtIndex:index];
+    return [self _objectAtIndex:index withPagination:YES];
 }
 
 - (id)objectAtIndex:(NSUInteger)index withTriggerPagination:(BOOL)pagination {
-	if (pagination) {
-		return [self _objectAtIndex:index];
-	}
-
-	return [self.arrItems objectAtIndex:index];
+    return [self _objectAtIndex:index withPagination:pagination];
 }
 
-- (id)_objectAtIndex:(NSUInteger)index {
-	if (index == self.arrItems.count - 1) {
-		[self _setShouldLoadDataForNextPage];
-	}
+- (id)_objectAtIndex:(NSUInteger)index withPagination:(BOOL)pagination {
+    if (pagination && index == self.arrItems.count - 1) {
+        [self _setShouldLoadDataForNextPage];
+    }
 
-	return [self.arrItems objectAtIndex:index];
+    return [self.arrItems objectAtIndex:index];
 }
 
 #pragma mark - Public method
@@ -75,7 +67,6 @@
 #pragma mark - Private methods
 
 - (void)_setShouldLoadDataForNextPage {
-
     if (self.isReachMaxPage) {
         return;
     }
@@ -88,32 +79,47 @@
 }
 
 - (void)_loadNextPage {
-
     self.isLoadingNextPage = YES;
 
-    id<KHLoadingOperationProtocol> loadingOperation = [self.delegate loadingOperationForSectionViewModel:self forPage:self.currentPage+1];
+    id <KHLoadingOperationProtocol> loadingOperation = [self.delegate loadingOperationForSectionViewModel:self forPage:self.currentPage + 1];
     self.loadingOperation = loadingOperation;
     __weak typeof(self) weakSelf = self;
+
     [loadingOperation loadData: ^(NSArray *data, NSError *error) {
         [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
-            [weakSelf _dataOperation:loadingOperation finishedLoadingForPage:self.currentPage+1 error:error];
+            [weakSelf _dataOperation:loadingOperation finishedLoadingForPage:self.currentPage + 1 error:error];
         }];
     }];
 }
 
-- (void)_dataOperation:(NSBlockOperation<KHLoadingOperationProtocol> *)operation finishedLoadingForPage:(NSUInteger)page error:(NSError *)error{
+- (void)_dataOperation:(NSBlockOperation <KHLoadingOperationProtocol> *)operation finishedLoadingForPage:(NSUInteger)page error:(NSError *)error {
     self.isLoadingNextPage = NO;
 
     if (operation.dataPage.count == 0 && !error) {
         self.isReachMaxPage = YES;
-    } else {
-        self.currentPage ++;
+    }
+    else {
+        self.currentPage++;
     }
 
-    [self.arrItems addObjectsFromArray:operation.dataPage];
+    NSArray *arr = operation.dataPage;
+    if (self.reversed) {
+        arr = [self _reverseArray:operation.dataPage];
+    }
+
+    [self.arrItems addObjectsFromArray:arr];
     if ([self.delegate respondsToSelector:@selector(dataProvider:didLoadDataAtPage:withItems:error:)]) {
         [self.delegate dataProvider:self didLoadDataAtPage:self.currentPage withItems:operation.dataPage error:error];
     }
+}
+
+- (NSArray *)_reverseArray:(NSArray *)array {
+    NSMutableArray *resultArray = [NSMutableArray array];
+    for (int i = array.count - 1; i >= 0; i--) {
+        [resultArray addObject:array[i]];
+    }
+    
+    return [resultArray copy];
 }
 
 @end
